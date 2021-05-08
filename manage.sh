7029b878ec743da25e349e7c97ee573e7c2fbb77
Martin <33915290+ItsMCB@users.noreply.github.com>
2021-05-08 15:35:52 -0500

    Better formatting
    Forgot to update this repo... oops. Fixed an issue where inputs straight up wouldn't work. Updating a server should work but I haven't tested it in a hot minute. There are other changes like build info for the script, tmux, etc. Did a bit more testing of what I could with it too. Enjoy :D

--- manage.sh
+++ manage.sh
@@ -5,23 +5,28 @@
 
 
 SERVER_NAME="SouthHollow"
-SERVERS_DIR="/home/Servers"
+SERVERS_DIR="/home/Servers/"
+TEMPLATSERVERS_DIR="/home/ServerTemplates/"
+BACKUP_DIR="/home/ServerBackup/"
+
 
 
 # DO NOT EDIT ANYTHING BELOW THIS.
-VERSION=1.0
+BUILD=2
 
-function home {
+home() {
     clear
     echo "===== ${SERVER_NAME} - $(date)  ====="
-    read -n 1 -p "Manage Server (m/s) | Check for Updates (u) | Close (q) " ans;
+    read -p "Manage a Server or Tmux Session (m/s) | Check for Manage.sh Updates (u) | Close (q) " ans;
     case $ans in
         m|M|s|S)
             manageserver;;
         u|U)
             clear;
-            echo "Version: ${VERSION} | Update feature coming soon!";
-            read;
+            echo "Build: ${BUILD}";
+            echo "Tmux Version:"
+            tmux -V
+            read temp; # Pause
             home;;
         d|D)
             debug;;
@@ -32,19 +37,40 @@ function home {
     esac
 }
 
-function manageserver {
+manageserver() {
     clear
     echo "===== ${SERVER_NAME} - $(date)  ====="
-    read -n 1 -p "Select Server (s) | Open New Session (n) | Update Pre-existing Server (u) | Create New Server (c) | Go Back (q) " ans;
+    echo "Select Server (s)"
+    echo "Open a New Tmux Session (n)"
+    echo "Update Pre-existing Server From a Template (u)"
+    echo "Create New Server (c)"
+    read -p "Go Back (q) " ans;
     case $ans in
-        s|S)
-            echo "Select server moment";;
+        s|S) # Select Server
+            echo "--------------------"
+            tmux ls
+            echo "--------------------"
+            echo "Which server would you like to select? | q to cancel"
+            read server
+            if [ $server = "q" ]
+            then
+            manageserver;
+            else
+            tmux a -t $server;
+            fi;;
         n|N)
-            echo "Open new session moment";;
+            echo "What would you like the session to be called?"
+            read sessionname
+            if [ $sessionname = "q" ]
+            then
+            manageserver;
+            else
+            tmux new -s $sessionname
+            fi;;
         c|C)
             echo "Create new server moment";;
         u|U)
-            echo "Update pre-existing server moment";;
+            updateservertemplate;;
         q|Q)
             home;;
         *)
@@ -53,10 +79,56 @@ function manageserver {
 }
 
 
-function debug {
+
+updateservertemplate() {
     clear
-    echo "===== ${SERVER_NAME} - $(date)  ====="
-    read -n 1 -p $'Debug Menu.... Press q to go back\n' ans;
+    echo "===== ${SERVER_NAME} - Updater  ====="
+    echo "Which server would you like to update? - (q to cancel)"
+    read updatethisserver
+    if [ ${updatethisserver} = "q" ]; then
+    manageserver;
+    elif [ -z ${updatethisserver} ]; then
+    echo "BAD IDEA. You understand you're targeting the whole server directory, right? Don't worry. I'll reset your mistake."
+    read temp
+    updateservertemplate
+    else
+    # Wants to update a server through a template
+    echo "You want to update '${updatethisserver}', correct? (y/n)"
+    read confirmupdateservertemplate
+    if [ $confirmupdateservertemplate = "n" ]
+    then
+    updateservertemplate;
+    else
+    TEMPLATESERVERLOC=${TEMPLATSERVERS_DIR}${updatethisserver} #Template Serv Loc
+    ORIGINSERVERLOC=${SERVERS_DIR}${updatethisserver} # Production Serv Loc
+    BACKUP_DIR=${BACKUP_DIR}${updatethisserver} # Backup Loc
+    cp -R ${ORIGINSERVERLOC}* ${BACKUP_DIR} # Make a quick backup to avoid catastrophes 
+    echo "Made a copy of ${ORIGINSERVERLOC} to ${BACKUP_DIR}"
+    echo "Now removing files from ${ORIGINSERVERLOC}..."
+    rm -r ${ORIGINSERVERLOC} # Deletes the origin folder
+    echo "Writing over data for ${ORIGINSERVERLOC} from ${TEMPLATESERVERLOC}..."
+    cp -R ${TEMPLATESERVERLOC}* ${SERVERS_DIR} # Copy template to production
+    echo "Done! Press any key to return."
+    read temp
+    manageserver
+    fi
+    fi
+}
+
+
+
+
+
+
+
+
+
+# Testing out interesting things I can do in bash
+
+debug(){
+    clear
+    echo "===== ${SERVER_NAME} - Debug Menu - $(date)  ====="
+    read -p $'U | Q' ans;
     case $ans in
         u|U)
             for i in 1 2 3 4 5;
@@ -71,38 +143,4 @@ function debug {
 }
 
 
-home
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Testing, ignore.
-PS3='Please enter your choice: '
-options=("Server Manager" "Bungee Restart" "Option 3 y33t" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Server Manager")
-            echo "you chose choice 1"
-            ;;
-        "Bungee Restart")
-            echo "you chose choice 2"
-            ;;
-        "Option 3 y33t")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "Hey, $REPLY is an invalid option";;
-    esac
-done
+home # Call the home to open
